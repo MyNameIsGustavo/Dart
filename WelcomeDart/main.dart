0e65fe0350befcbfac2044537074fedba2446857
Gustavo Rocha <87160095+MyNameIsGustavo@users.noreply.github.com>
2023-01-14 23:24:54 -0300

    atualizando as anotações.

--- WelcomeDart/main.dart
+++ WelcomeDart/main.dart
@@ -1,137 +1,116 @@
-/*
-void main() {
-
- //inteiro numero sem virgula
-  int numero1=10;
+/*Introdução ao Dart. - Tipos de dados.*/
+void main() 
+{ //Inteiros;
+  int numero1 = 10;
   int numero2 = 2;
   
   print("$numero1");
   print("o conteudo de numero 1 é $numero1");
   
-  //double
+  //Numeros com virgula;
   double numero3 = 1.99;
-  double numero4=0.00000001234;
-  
+  double numero4 = 0.00000001234;
   print("valores $numero3 e $numero4");
   
-  //texto 
+  //Textos; 
   String cidade = "Sorocaba";
   String nome = "Fit";
-  
   print("O $nome fica em $cidade");
-  
-  
-  //boorleano - true e false
+
+  //Booleanos;
   bool status = false;
   print("$status");
+
   status = true;
   print("$status");
   
+  //Variaveis dinamicas;
+  dynamic vDinamicas = 0;
+  vDinamicas = 2.987;
+  vDinamicas = "To ligado";
+  vDinamicas = true;
   
-  //dinamico
-  
-  dynamic qualquer= 0;
-  qualquer = 2.987;
-  qualquer = "To ligado";
-  qualquer = true;
-  
-  
+  //Listas;  
   List <int> valores = [0,1,2,3,4];
   print(valores);
   print(valores[4]);
   valores.add(100);
   print(valores);
-  
-    
 }
 
+/*Dart - Operadores.*/
+void main() 
+{ //Operadores;
 
-dart  - operadores
-void main() {
-//operadores
- //inteiro numero sem virgula
-  int numero1=10;
+  int numero1 = 10;
   int numero2 = 2;
   int total;
   int result;
- //soma  +
   total = numero1 + numero2;
-  
   print("$numero1 + $numero2 = $total");
   print(result=numero1+numero2);
   
-   
-  //subtração  - 
-   total = numero1 - numero2;
-  
+  //Subtração  - 
+  total = numero1 - numero2;
   print("$numero1 - $numero2 = $total");
     
-    //multiplicação  *
-   total = numero1 * numero2;
-  
+  //Multiplicação  *
+  total = numero1 * numero2;
   print("$numero1 * $numero2 = $total");
   
-  //divisão /
+  //Divisão /
   double resultado;
-   resultado = numero1 / numero2;
-  
+  resultado = numero1 / numero2;
   print("$numero1 / $numero2 = $resultado");
 }
 
-dart estrutura condicional
-void main() {
-int numero1= 10;
-int numero2 = 200;
-  //condicionais
+/*Dart - Estrutura Condicional.*/
+void main() 
+{ int numero1 = 10;
+  int numero2 = 200;
+  //Condicionais;
   if(numero1 > numero2 )
-  {
     print("$numero1 é maior que $numero2");
-  }else{
+  else
     print("$numero1 é menor que $numero2");
-  }
-  
-  
 }
 
-dart - funções
-void main() {
-  int numero1= 10;
+/*Dart - Funções.*/
+void main() 
+{ int numero1 = 10;
   int numero2 = 2;
   int total;
+
   print("Começa aqui");
   
   soma();
+
   sub(numero1, 6);
+
   total = mult();
+
   print(total);
-  
   print("Fim do programa");
-  
 }
 
-//funções sem retorno e sem parametro
+/*Dart - Funções sem retorno e sem parametro.*/
 void soma()
-{
-  int a=100;
-  int b= 10;
-  int t=a+b;
+{ int a = 100;
+  int b = 10;
+  int t= a + b;
   print("$a + $b = $t");
 }
 
-//função sem retorno com parametro
+/*Dart - Função sem retorno com parametro.*/
 void sub(int n1, int n2)
-{
-  int  resp = n1 - n2;
+{ int  resp = n1 - n2;
   print("$n1 - $n2 = $resp");
 }
 
-
-//função com retorno sem parametro
+/*Dart - Função com retorno sem parametro.*/
 int mult()
-{
-  int z=20;
-  int x=10;
-  int c = z*x;
+{ int z = 20;
+  int x = 10;
+  int c = z * x;
   return c;
 }
-*/
